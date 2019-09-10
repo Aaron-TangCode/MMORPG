d052bb325bed1ce53181f958eab5f472653beb1e
TangHaiLin <1025741620@qq.com>
2019-09-10 11:41:25 +0800

    Demo-v1.8.4-添加自动生成类的脚步文件（linux&windows）

---
+++ serProbuf.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+protoc --java_out=src\main\java src\main\java\com\game\protobuf\Map.proto
+protoc --java_out=src\main\java src\main\java\com\game\protobuf\Goods.proto
+protoc --java_out=src\main\java src\main\java\com\game\protobuf\Npc.proto
+protoc --java_out=src\main\java src\main\java\com\game\protobuf\Role.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgAuctionInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgBossInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgChatInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgEmailInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgEquipInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgGangInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgGoodsInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgMapInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgNpcInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgRankInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgRoleInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgShopInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgSkillInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgTaskInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgTradeInfo.proto
+protoc --proto_path=src\main\java\com\game\protobuf --java_out=src\main\java src\main\java\com\game\protobuf\MsgUserInfo.proto
